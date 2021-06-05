93ea5a43cba0b639780ae61d71ad8edd77020b5b
shivansh218 <71941868+shivansh218@users.noreply.github.com>
2021-06-05 20:18:37 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+
+var curryear = new Date().getFullYear();
+
+return (
+
+<footer>
+
+<p>Copyright @ {curryear}</p>
+
+</footer>
+
+);
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing Bootcamp taken by Shaurya Sinha.We covered
+        everything from Scratch including Javascript,React.js,HTML This Bootcamp
+        is 7 days Long and I really enjoy the Bootcamp and I learn lots of
+        things from this bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Note;
